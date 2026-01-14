c8ef0fbf1fdd72aca395657cd001ee0382de748e
Cassitly <102461124+cassitly@users.noreply.github.com>
2026-01-14 05:33:23 +0000

    Update .gitignore to exclude data directory and add errors log file for tracking runtime issues

---
+++ splca/models/__init__.py
@@ -0,0 +1,9 @@
+from .vision import VisionClassifier
+from .audio import AudioClassifier
+from .text import TextClassifier
+
+__all__ = [
+ "VisionClassifier",
+ "AudioClassifier",
+ "TextClassifier"
+]
